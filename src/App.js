c600d833e1170d0c6a0661d81d7015e2eef2ef61
Adarsh-1011 <adarshnayak1011@gmail.com>
2024-05-17 12:13:17 +0530

    interacted with weather API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Registration from "./pages/registration/Registration";
 import GenrePage from "./pages/genre/GenrePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import HomePage from "./pages/homepage/Homepage";
+import HomePage from "./pages/homepage/Homepage";
 // import Dashboard from "./pages/dashboard/Dashboard";
 // import PromotionPage from "./pages/promotion/PromotionPage";
 
@@ -12,8 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Registration />} />
           <Route path="/genre" element={<GenrePage />} />
-          {/* <Route path="/homepage" element={<HomePage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/homepage" element={<HomePage />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/promotion" element={<PromotionPage />} /> */}
 
         </Routes>
